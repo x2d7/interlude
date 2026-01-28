af9b4251445ecab00cab69cca5c728c337d19ab8
2xd7 <cyrsiadv@gmail.com>
2026-01-28 15:31:24 +0400

    chore(types): clarify Close() method behavior in Stream interface

--- types/streaming.go
+++ types/streaming.go
@@ -4,5 +4,5 @@ type Stream[T any] interface {
     Next() bool   // advance; returns false on EOF or error
     Current() T   // the current element; valid only if Last Next() returned true
     Err() error   // non-nil if the stream ended because of an error
-    Close() error // release resources
+    Close() error // release resources, ensure Next() returns false
 }
