73bd838306c90ed94dfbbe8ba3f7e060e42e87ea
2xd7 <cyrsiadv@gmail.com>
2026-02-18 18:25:09 +0400

    chore: go fmt

--- chat/tools/errors.go
+++ chat/tools/errors.go
@@ -3,6 +3,6 @@ package tools
 import "errors"
 
 var (
-	ErrEmptyToolID      = errors.New("tool ID cannot be empty")
+	ErrEmptyToolID       = errors.New("tool ID cannot be empty")
 	ErrToolAlreadyExists = errors.New("tool with this ID already exists")
 )
